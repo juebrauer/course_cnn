cbd4850165d2028b2ddf3bd9dc3918fb65f42457
juebrauer on DL server <juebrauer@gmail.com>
2020-05-14 14:43:20 +0200

    Changes maded for experiments running on DL server

--- solutions_for_exercises/06_importance_of_start_weights/all_experiments.sh
+++ solutions_for_exercises/06_importance_of_start_weights/all_experiments.sh
@@ -2,7 +2,7 @@
 
 echo "Started all experiments."
 
-for seed_value in {02..02}
+for seed_value in {01..30}
 do
   python3 single_experiment.py $seed_value > logfile_$seed_value.txt
   #python3 single_experiment.py $seed_value
