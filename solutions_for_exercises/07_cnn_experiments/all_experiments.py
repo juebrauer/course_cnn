64443ee3729e42f48d6855f7418e94da53e6d143
Juergen Brauer <juebrauer@gmail.com>
2020-05-15 16:18:41 +0200

    Moved run loop to outer loop

--- solutions_for_exercises/07_cnn_experiments/all_experiments.py
+++ solutions_for_exercises/07_cnn_experiments/all_experiments.py
@@ -5,16 +5,16 @@ print("all_experiments.py started.")
 dataset_name = "imagenette"
 
 exp_nr = 0
-for cnn_name in ["same-nr-filters", "inc-nr-filters"]:
-    for nr_train_imgs_per_class in [100,200,300,400,500,600,700,800,900]:
-        for run in [1,2,3]:
-            exp_nr +=1
-            exp_name = "{0:0>3}".format(exp_nr)
+for run in [1,2,3]:
+    for cnn_name in ["same-nr-filters", "inc-nr-filters"]:
+        for nr_train_imgs_per_class in [100,200,300,400,500,600,700,800]:
+                exp_nr +=1
+                exp_name = "{0:0>3}".format(exp_nr)
 
-            cmd = "python3 single_experiment_traindatasize.py {0} {1} {2} {3} > logfile_{4}.txt"\
-                  .format(exp_name, cnn_name, dataset_name, nr_train_imgs_per_class, exp_name)
+                cmd = "python3 single_experiment_traindatasize.py {0} {1} {2} {3} > logfile_{4}.txt"\
+                      .format(exp_name, cnn_name, dataset_name, nr_train_imgs_per_class, exp_name)
 
-            os.system( cmd )
+                os.system( cmd )
 
 
 print("all_experiments.py finished.")
