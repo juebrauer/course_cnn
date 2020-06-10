45384cd227c8c95dec6cdaf8ea636989efd4cb08
juebrauer on DL server <juebrauer@gmail.com>
2020-06-10 14:59:41 +0200

    Bugfix: exp_nr was not incremented after each experiment with a catastrophic result: old history files were overwritten!

--- solutions_for_exercises/07_cnn_experiments/all_experiments_random_weights.py
+++ solutions_for_exercises/07_cnn_experiments/all_experiments_random_weights.py
@@ -3,11 +3,14 @@ import os
 print("all_experiments_random_weights.py started.")
 
 
+exp_nr = 0
 for dataset_name in ["imagenette", "imagewoof"]:
 
-    for exp_nr in range(1,6):
+    for run in range(1,11):
 
         for use_rnd_weights in [True, False]:
+
+            exp_nr += 1
 
             exp_name = "{0:0>4}".format(exp_nr)
 
